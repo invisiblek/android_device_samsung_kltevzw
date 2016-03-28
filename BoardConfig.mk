9317b791852844c16cc64eb34be176797581ed56
Ethan Chen <intervigil@gmail.com>
2016-03-27 21:43:32 -0500

    kltevzw: Convert init extension to C++
    Change-Id: Idc304d6a092bab2e82245992afb68a1595c44472

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_vzw_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/samsung/kltevzw/init/init_klte.c
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/kltevzw/init/init_klte.cpp
 TARGET_UNIFIED_DEVICE := true
 
 # NFC
